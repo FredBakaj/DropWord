3b5d06fd4c56ccac11b57fc9e54fc49ad849781d
Федор Бакай <f.bakai@avto.pro>
2024-03-03 15:07:49 +0200

    added to SendTextMessageMarkdown2Async new character replacement

--- src/TgBot/Core/Extension/TelegramBotClientExtension.cs
+++ src/TgBot/Core/Extension/TelegramBotClientExtension.cs
@@ -10,11 +10,21 @@ public static class TelegramBotClientExtension
     public static async Task<Message> SendTextMessageMarkdown2Async(this ITelegramBotClient client, long chatId, string message,
         IReplyMarkup? replyMarkup = null)
     {
-        message = message.Replace("-", "\\-");
-        message = message.Replace("#", "\\#");
-        message = message.Replace(".", "\\.");
+        message = message.Replace("[", "\\[");
+        message = message.Replace("]", "\\]");
+        message = message.Replace("(", "\\(");
+        message = message.Replace(")", "\\)");
+        message = message.Replace("`", "\\`");
         message = message.Replace(">", "\\>");
-        message = message.Replace("<", "\\<");
+        message = message.Replace("#", "\\#");
+        message = message.Replace("+", "\\+");
+        message = message.Replace("-", "\\-");
+        message = message.Replace("=", "\\=");
+        message = message.Replace("{", "\\{");
+        message = message.Replace("}", "\\}");
+        message = message.Replace(".", "\\.");
+        message = message.Replace("!", "\\!");
+
         return await client.SendTextMessageAsync(chatId, message, replyMarkup: replyMarkup, parseMode: ParseMode.MarkdownV2);
     }
 }
